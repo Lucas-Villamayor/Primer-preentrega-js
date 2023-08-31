ba7ac8914e535cf31cf0789f85dfacb1f87ae108
Lucas Villamayor <luucasnahuel20@gmail.com>
2023-08-30 22:02:04 -0300

    first commit

---
+++ Javascript/main.js
@@ -0,0 +1,37 @@
+let usser = prompt("Ingrese su nombre de usuario");
+let pass = prompt("Ingrese su contraseña")
+let fecha = parseInt(prompt("Ingrese año de nacimiento"));
+
+let usser1 = "Lucas_coder";
+let pass1 = 25901;
+let fecha1 = 1994;
+
+
+ while (usser != usser1){
+    alert ("Usuario incorrecto")
+ }
+ if (pass != pass1){
+    alert ("contraseña incorrecta")
+ }
+ if (fecha == (2023 - "fecha1")){
+    alert ("No ingresaste bien los datos")
+    return;
+ } else if(usser == usser1 && pass == pass1 && fecha == fecha1){
+    console.log ("Bienvenido Lucas")
+ }
+
+
+
+
+
+ function ingreso () {
+
+ if (usuario != "" && password != "" && edad1 != "" && usuario === usuario1 && password1 === password  && edad +(edad - edad1)){
+     console.log("Bienvenido")
+    
+ } else{ alert ("Los datos no son correctos, intente nuevamente");
+ }
+ }
+
+let edad = (5 * 2);
+console.log("edad")
